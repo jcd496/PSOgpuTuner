f3a06d144d199cb461b5825f732c80b331f81bec
jcd496 <jcd496@nyu.edu>
2019-03-11 11:24:18 -0400

    gpu kernels in header

---
+++ src/cuda_kernel.cu
@@ -0,0 +1,10 @@
+#include <cublas_v2.h>
+#include <iostream>
+
+
+using namespace std;
+void gemm_kernel(const float *A, const float *B, float *C, const int m, const int k, const int n){
+	cout<< "hi"<<endl;
+
+
+}
---
+++ src/cuda_kernel.hpp
@@ -0,0 +1,3 @@
+
+
+void gemm_kernel(const float *A, const float *B, float *C, const int m, const int k, const int n);
