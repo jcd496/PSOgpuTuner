d6d3ba8f8512ac231628ef714993ee369b60fd88
jcd496 <jcd496@nyu.edu>
2019-03-13 13:26:13 -0400

    gemm signature

--- src/cuda_kernel.hpp
+++ src/cuda_kernel.hpp
@@ -1,3 +1,12 @@
+#include "cublas_v2.h"
+#ifndef CUDA_KERNELS
+#define CUDA_KERNELS
+
+void kernel_wrapper(int iteration, dim3 blocksPerGrid, dim3 threadsPerBlock, struct param_time_record * records);
+void gemm_kernel(const float *A, const float *B, float *C, const int n, const int k, const int m);
 
 
-void gemm_kernel(const float *A, const float *B, float *C, const int m, const int k, const int n);
+
+
+
+#endif
