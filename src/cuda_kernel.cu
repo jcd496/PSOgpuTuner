a177f5290ed5770812367866853f798c5271cda4
jcd496 <jcd496@nyu.edu>
2019-03-14 14:13:48 -0400

    jacobi solver started

--- src/cuda_kernel.cu
+++ src/cuda_kernel.cu
@@ -17,7 +17,43 @@ __global__ void gemm_kernel(const double *A, const double *B, double *C, const i
 			C[row*m+col] = C[row*m+col] + A[row*k+p]*B[p*k+col];
 
 }
-
+__device__ double residual(double *U, double *A ,double *F, int N){
+	int i, j;
+	double residual = 0.0;
+	for(i=1;i<=N; i++){
+		double element = 0.0;
+		for(j=0;j<N;j++)
+			element += A[j+i*N]*U[j];
+		residual+=element*element;
+	}
+	return sqrtf(residual);
+}
+__global__ void jacobi_kernel(double *U, double *A, double *F, int N){
+	double tolerance =1e-6;
+	int MAX_ITER = 1000;
+	double residual_original, residual_cur;
+	residual_original = residual(U,A,F,N);
+	residual_cur = residual_original;
+	int j, iter;
+	iter = 0;
+	while(iter<MAX_ITER && residual_cur/residual_original > tolerance){
+		//int j = blockIdx.x*blockDim.x + threadIdx.x; ONE DIMENSIONAL BLOCKS??
+		int i = blockIdx.x*blockDim.x + threadIdx.x;
+		//for(i=0;i<N;i++){
+			double sigma = 0.0;
+			for(j=0; j<N; j++){
+				if(j!=i)
+					sigma+=A[j+i*N]*U[j];
+			}
+			U[i]= 1/A[i+i*N]*(F[i]-sigma);
+		//}
+		__syncthreads();
+		if(i==1)
+			residual_cur = residual(U,A,F,N);
+		__syncthreads();
+		iter++;
+	}
+}
 
 //PSO will call kernel_wrapper with different parameters and kernel_wrapper will evaluate kernel and store statistics
 void kernel_wrapper(int iteration, dim3 blocksPerGrid, dim3 threadsPerBlock, record_t * records){
@@ -72,7 +108,34 @@ void kernel_wrapper(int iteration, dim3 blocksPerGrid, dim3 threadsPerBlock, rec
 	cout<<"GEMM\n"<<"Error "<<checksum-sum<<endl;
 	cudaEventElapsedTime(&records[iteration].gemm, gemm_start, gemm_stop);
 	cout<<"time "<< records[iteration].gemm/1e3<< "seconds"<<endl;
-	cudaFree(A_d);
+	
+	//JACOBI
+	double *U, *F, *U_d, *F_d;
+	U = (double *)malloc(m*sizeof(double)), F = (double *)malloc(m*sizeof(double));
+	for(int i=0; i<m; i++){
+		U[i]=0.0;
+		F[i]=1.0;
+	}
+	cudaMalloc(&U_d, m*sizeof(double));
+	cudaMemcpy(U_d, U, m*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMalloc(&F_d, m*sizeof(double));
+	cudaMemcpy(F_d, F, m*sizeof(double), cudaMemcpyHostToDevice);
+	//KERNEL, TIME, MEMCPY
+	cudaEvent_t jacobi_start, jacobi_stop;
+	cudaEventCreate(&jacobi_start), cudaEventCreate(&jacobi_stop);
+	cudaEventRecord(jacobi_start, 0);
+	jacobi_kernel<<<blocksPerGrid, threadsPerBlock>>>(U, C, F, m);
+	cudaEventRecord(jacobi_stop, 0);
+	cudaMemcpy(U, U_d, m*sizeof(double), cudaMemcpyDeviceToHost);
+	cudaEventSynchronize(jacobi_stop);
+	cout<<"JACOBI\n"<<"ERROR "<<endl;
+	//cudaEventElapsedTime(&records[iteration].jacobi, jacobi_start, jacobi_stop);
+	//cout<<"time "<< records[iteration].jacobi/1e3<< "seconds"<<endl;
+	
+
+
+
+	cudaFree(A_d);//reuse C for jacobi
 	cudaFree(B_d);
 	cudaFree(C_d);
 	free(A), free(B), free(C);
