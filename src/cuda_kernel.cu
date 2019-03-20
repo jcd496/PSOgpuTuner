489aa6eecd858904e54c2af8ab36eb6344565390
jcd496 <jcd496@nyu.edu>
2019-03-20 19:52:59 -0400

    jacobi bug, host and gpu dont match

--- src/cuda_kernel.cu
+++ src/cuda_kernel.cu
@@ -27,78 +27,67 @@ __global__ void gemm_kernel(const double *A, const double *B, double *C, const i
 
 }
 //RESIDUAL ERROR USED IN JACOBI SMOOTHING
-__host__ __device__ double residual(double *U, double *A ,double *F, int N){
+double residual(double *U, double *A ,double *F, int N){
 	int i, j;
 	double residual = 0.0;
-	for(i=1;i<=N; i++){
+	for(i=0;i<N; i++){
 		double element = 0.0;
 		for(j=0;j<N;j++)
 			element += A[j+i*N]*U[j];
-		residual+=element*element;
+		residual+=(element-F[i])*(element-F[i]);
 	}
-	return sqrtf(residual);
+	return sqrt(residual);
 }
-//JACOBI SMOOTHING KERNEL.  NO OPTIMIZATIONS, MAY INCLUDE TILING ETC
 __global__ void jacobi_kernel(double *U, double *Unew, double *A, double *F, int N){
-	//AxU=F
-	double tolerance =1e-6;
-	int MAX_ITER = 1000;
-	double residual_original, residual_cur;
-	residual_original = residual(U,A,F,N);
-	residual_cur = residual_original;
-	int iter;
-	iter = 0;
-	while(iter<MAX_ITER && residual_cur/residual_original > tolerance){
-		//int j = blockIdx.x*blockDim.x + threadIdx.x; ONE DIMENSIONAL BLOCKS??
-		int i = blockIdx.x*blockDim.x + threadIdx.x;
-		//for(int i=0;i<N;i++){
-			double sigma = 0.0;
-			for(int j=0; j<N; j++){
-				if(j!=i)
-					sigma+=A[j+i*N]*U[j];
-			}
-			Unew[i]= 1/A[i+i*N]*(F[i]-sigma);
-		//}
-		__syncthreads();
-		if(i==1)
-			residual_cur = residual(U,A,F,N);
-		__syncthreads();
-		double * temp = U;
-		U = Unew;
-		Unew = temp;
-		iter++;
-	}
-}
-//JACOBI SMOOTHING EXECUTED ON HOST TO HANDLE ERRORS
-double jacobi_host(double *U, double *A, double *F, int N){
-	double tolerance =1e-6;
-	int MAX_ITER = 1000;
-	double residual_original, residual_cur;
-	residual_original = residual(U,A,F,N);
-	residual_cur = residual_original;
-	int iter;
-	iter = 0;
-	double * Unew = (double *)malloc(N*sizeof(double));
-	for(int i=0;i<N;i++) Unew[i]=0.0;
-	while(iter<MAX_ITER && residual_cur/residual_original > tolerance){
-		for(int i=0;i<N;i++){
-			double sigma = 0.0;
-			for(int j=0; j<N; j++){
-				if(j!=i)
-					sigma+=A[j+i*N]*U[j];
-			}
-			Unew[i]= 1/A[i+i*N]*(F[i]-sigma);
-		}
-		residual_cur = residual(U,A,F,N);
-		double *temp = U;
-		U = Unew;
-		Unew = temp;
-		iter++;
-	}
+	int i = blockIdx.x*blockDim.x + threadIdx.x;
+	int y = blockIdx.y*blockDim.y + threadIdx.y;
+	//for(int i=0;i<N;i++){
+	double sigma = 0.0;
 
+	if(i<N && y==0){
+		for(int j=0; j<N; j++){
+			if(j!=i)
+				sigma+=A[j+i*N]*U[j];
+		}
+		Unew[i] = (F[i]-sigma)/A[i+i*N];
+	}
+	//}
+}
+//JACOBI SMOOTHING EXECUTED ON HOST/DEVICE TO MEASURE ERRORS
+double jacobi_smoother(double *U, double *Unew, double *A, double *F, int N, dim3 blocksPerGrid, dim3 threadsPerBlock, bool KERNEL){
+	int MAX_ITER = 1000;
+	int iter=0;
+	double *temp;
+	while(iter<MAX_ITER){
+		if(KERNEL){
+			//printf("%d %d\n", U, Unew);
+			jacobi_kernel<<<blocksPerGrid, threadsPerBlock>>>(U, Unew, A, F, N);
+		}else{
+			printf("%d %d\n", U, Unew);
+			
+			for(int i=0;i<N;i++){
+				double sigma = 0.0;
+				for(int j=0; j<N; j++){
+					if(j!=i)
+						sigma+=A[j+i*N]*U[j];
+				}
+				Unew[i]= (F[i]-sigma)/A[i+i*N];
+			}
+
+		}
+		temp = U;
+		U = Unew;
+		Unew = temp;
+		iter++;
+	}
+	if(KERNEL)
+		return 0.0;
 	double jacobi_error =0.0;
-	for(int i=0;i<N;i++) jacobi_error+=U[i];
-	free(Unew);
+	for(int i=0;i<N;i++){
+		jacobi_error+=U[i];
+		printf("%lf ", U[i]);
+	}
+	printf("\n");
 	return jacobi_error;
 }
 
@@ -117,13 +106,14 @@ void kernel_wrapper(int iteration, dim3 blocksPerGrid, dim3 threadsPerBlock, rec
 	A = (double *)malloc(n*k*sizeof(double)), B= (double *)malloc(k*m*sizeof(double)), C=(double *)malloc(n*m*sizeof(double));
 	for(int i=0;i<n;i++)
 		for(int j=0;j<k;j++)
-			A[i*k+j]=2.0*i+1.0*j;
+			A[i*k+j]=2.0*i+1.0*j+1.0;
 	for(int i=0;i<k;i++)
 		for(int j=0;j<m;j++)
-			B[i*m+j]=2.0*i+1.0*j;
+			B[i*m+j]=2.0*i+1.0*j+1.0;
 	for(int i=0;i<n;i++)
 		for(int j=0;j<m;j++)
 			C[i*m+j]=0.0;
+	
 	double gemm_checksum = 0.0;
 	for(int i=0;i<n;i++)
 		for(int p=0;p<k;p++)
@@ -138,15 +128,17 @@ void kernel_wrapper(int iteration, dim3 blocksPerGrid, dim3 threadsPerBlock, rec
 	gpuErrchk(cudaMemcpy(B_d, B, k*m*sizeof(double), cudaMemcpyHostToDevice)); 
 	gpuErrchk(cudaMalloc(&C_d, n*m*sizeof(double)));
 	gpuErrchk(cudaMemcpy(C_d, C, n*m*sizeof(double), cudaMemcpyHostToDevice)); 
+	
 	//LAUNCH KERNEL, RECORD TIME, COPY KERNEL RESULTS TO HOST
 	cudaEvent_t gemm_start, gemm_stop;
 	gpuErrchk(cudaEventCreate(&gemm_start));
 	gpuErrchk(cudaEventCreate(&gemm_stop));
-	gpuErrchk(cudaEventRecord(gemm_start,0));
+	gpuErrchk(cudaEventRecord(gemm_start));
 	gemm_kernel<<<blocksPerGrid,threadsPerBlock>>>(A_d, B_d, C_d, n, k, m);
-	gpuErrchk(cudaEventRecord(gemm_stop,0));
+	gpuErrchk(cudaEventRecord(gemm_stop));
 	gpuErrchk(cudaMemcpy(C, C_d, n*m*sizeof(double), cudaMemcpyDeviceToHost));
 	gpuErrchk(cudaEventSynchronize(gemm_stop));
+	
 	//CALCULATE ERROR
 	for(int i =0;i<n;i++)
 		for(int j=0; j<m; j++)
@@ -155,54 +147,76 @@ void kernel_wrapper(int iteration, dim3 blocksPerGrid, dim3 threadsPerBlock, rec
 	cout<<"GEMM\n"<<"Error "<<gemm_checksum<<endl;
 	gpuErrchk(cudaEventElapsedTime(&records[iteration].gemm_time, gemm_start, gemm_stop));
 	cout<<"Time "<< records[iteration].gemm_time/1e3<< " Seconds"<<endl;
-	
+	cudaFree(A_d);
+	cudaFree(B_d);
+	cudaFree(C_d);
+	free(A), free(B), free(C);
+
+	//SYNCHRONIZE BETWEEN KENRNELS,  WORKS WITHOUT GEMM BUT CURRENTLY HAS RACE CONDITIONS
+	//gpuErrchk(cudaDeviceSynchronize());
+
 	//JACOBI
-	//INITIALIZE VECTORS U AND F, C MATRIX WILL BE REUSED FROM GEMM, CxU=F (nxm)x(mx1)=(nx1)
-	double *U, *F, *U_d, *F_d, *Unew_d;
-	U = (double *)malloc(m*sizeof(double)), F = (double *)malloc(n*sizeof(double));
+	//INITIALIZE VECTORS U AND F,  MATRIX J , JxU=F (nxm)x(mx1)=(nx1)
+	double *U, *F, *Unew,*U_d, *F_d, *Unew_d, *J_d;
+	U = (double *)malloc(m*sizeof(double));
+	Unew = (double *)malloc(m*sizeof(double));
+	F = (double *)malloc(n*sizeof(double));
 	for(int i=0; i<m; i++){
 		U[i]=0.0;
+		Unew[i]=0.0;
 		F[i]=1.0;
 	}
-	//cudaMemcpy(C_d, C, n*m*sizeof(double), cudaMemcpyHostToDevice); 
+	double *J=(double *)malloc(n*m*sizeof(double));
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(j==i-1 || j==i+1)
+				J[i*n+j]=-1.0;
+			else if(j==i)
+				J[i*n+j]=2.0;
+			else
+				J[i*n+j]=0.0;
+		}
+	}
+	//HOST SOLUTION
+	double jacobi_checksum = jacobi_smoother(U, Unew, J, F, m, blocksPerGrid, threadsPerBlock, false);
+	
 	gpuErrchk(cudaMalloc(&U_d, m*sizeof(double)));
 	gpuErrchk(cudaMalloc(&Unew_d, m*sizeof(double)));
 	gpuErrchk(cudaMemcpy(U_d, U, m*sizeof(double), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(Unew_d, Unew, m*sizeof(double), cudaMemcpyHostToDevice));
 	gpuErrchk(cudaMalloc(&F_d, n*sizeof(double)));
 	gpuErrchk(cudaMemcpy(F_d, F, n*sizeof(double), cudaMemcpyHostToDevice));
-	//HOST SOLUTION
-	double jacobi_checksum = jacobi_host(U, C, F, m);
+	gpuErrchk(cudaMalloc(&J_d, n*m*sizeof(double)));
+	cudaMemcpy(J_d, J, n*m*sizeof(double), cudaMemcpyHostToDevice); 
 
 	//KERNEL, TIME, MEMCPY
 	cudaEvent_t jacobi_start, jacobi_stop;
 	gpuErrchk(cudaEventCreate(&jacobi_start));
 	gpuErrchk(cudaEventCreate(&jacobi_stop));
-	gpuErrchk(cudaEventRecord(jacobi_start, 0));
-	jacobi_kernel<<<1, 1>>>(U_d, Unew_d, C_d, F_d, m);
-	//jacobi_kernel<<<blocksPerGrid, threadsPerBlock>>>(U, Unew_d, C, F, m);
-	gpuErrchk(cudaEventRecord(jacobi_stop, 0));
+	gpuErrchk(cudaEventRecord(jacobi_start));
+	for(int k=0;k<1000; k++){
+		//if(k%2==0)
+			jacobi_kernel<<<blocksPerGrid, threadsPerBlock>>>(U_d, Unew_d, J_d, F_d, m);
+		//else
+		//	jacobi_kernel<<<blocksPerGrid, threadsPerBlock>>>(Unew_d, U_d, J_d, F_d, m);
+	}
+	//jacobi_smoother(U_d, Unew_d, J_d, F_d, m, blocksPerGrid, threadsPerBlock, true);
+	gpuErrchk(cudaEventRecord(jacobi_stop));
 	gpuErrchk(cudaMemcpy(U, U_d, m*sizeof(double), cudaMemcpyDeviceToHost));
 	gpuErrchk(cudaEventSynchronize(jacobi_stop));
 	
-
 	//UPDATE CHECKSUM WITH GPU SOLUTION
-	for(int i=0;i<m;i++)
+	for(int i=0;i<m;i++){
+		printf("%lf ", U[i]);
 		jacobi_checksum-=U[i];
-
-
+	}
+	printf("\n");
 	cout<<"JACOBI\n"<<"Error "<<jacobi_checksum<<endl;
 	gpuErrchk(cudaEventElapsedTime(&records[iteration].jacobi_time, jacobi_start, jacobi_stop));
 	cout<<"Time "<< records[iteration].jacobi_time/1e3<< " Seconds"<<endl;
 	
 
-
-
-	cudaFree(A_d);
-	cudaFree(B_d);
-	cudaFree(C_d);
-	free(A), free(B), free(C);
-	free(U), free(F);
+	cudaFree(U_d), cudaFree(Unew_d), cudaFree(F_d), cudaFree(J_d);
+	free(U), free(Unew), free(F), free(J);
 }
-
-
 
