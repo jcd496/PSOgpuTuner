8dbd107727b60f3fadc7050d8db6dc6cc618a6b4
jcd496 <jcd496@nyu.edu>
2019-03-28 23:40:50 -0400

    added particle attributes

--- src/particle_struct.hpp
+++ src/particle_struct.hpp
@@ -5,8 +5,12 @@
 
 //SWARM IS ARRAY OF STRUCTURES
 typedef struct particle{
-	int blocks_per_thread[DIMENSION];
+	int blocks_per_grid[DIMENSION];
 	int threads_per_block[DIMENSION];
+	int best_block[DIMENSION];
+	int best_thread[DIMENSION];
+	int velocity_block[DIMENSION];
+	int velocity_thread[DIMENSION];
 	float gemm_time;
 	float jacobi_time;
 	float total_time;
